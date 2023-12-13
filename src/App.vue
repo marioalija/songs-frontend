13eb643e256ab9c6ce927e302663c00237349c7e
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-13 12:52:41 -0600

    index action

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      songs: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexSongs();
+  },
+  methods: {
+    indexSongs: function () {
+      axios.get("/songs.json").then((response) => {
+        console.log("songs index", response);
+        this.songs = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Songs</h1>
+    <div v-for="song in songs" v-bind:key="song.id">
+      <h2>{{ song.title }}</h2>
+      <p>Artist: {{ song.artist }}</p>
+      <p>Albun: {{ song.album }}</p>
+      <p>Duration: {{ song.duration }}</p>
+    </div>
   </div>
 </template>
 
