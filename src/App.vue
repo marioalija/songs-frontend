467265fa847da86ee148e5cbbba09b36d628ae6e
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-13 12:57:31 -0600

    create action

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       songs: [],
+      newSongParams: {},
     };
   },
   created: function () {
@@ -16,12 +17,36 @@ export default {
         this.songs = response.data;
       });
     },
+    createSong: function () {
+      axios
+        .post("/songs.json", this.newSongParams)
+        .then((response) => {
+          console.log("songs create", response);
+          this.songs.push(response.data);
+          this.newSongParams = {};
+        })
+        .catch((error) => {
+          console.log("songs create error", error.response);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <div class="home">
+    <h1>New Song</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newSongParams.title" />
+      Artist:
+      <input type="text" v-model="newSongParams.artist" />
+      Album:
+      <input type="text" v-model="newSongParams.album" />
+      Duration:
+      <input type="text" v-model="newSongParams.duration" />
+      <button v-on:click="createSong()">Create Song</button>
+    </div>
     <h1>All Songs</h1>
     <div v-for="song in songs" v-bind:key="song.id">
       <h2>{{ song.title }}</h2>
